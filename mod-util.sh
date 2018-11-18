8f51e9a22e13a7bd25da91aca26255adc4bdf9b4
Val Mandantes <mandantesv@gmail.com>
2018-11-18 15:43:52 +0800

    Bring back previous AB check
    Signed-off-by: Val Mandantes <mandantesv@gmail.com>

--- mod-util.sh
+++ mod-util.sh
@@ -9,22 +9,18 @@ MODUTILVER=v2.3
 MODUTILVCODE=23
 
 # Check A/B slot
-SLOT=`grep_cmdline androidboot.slot_suffix`
-if [ -z $SLOT ]; then
-  SLOT=_`grep_cmdline androidboot.slot`
-  [ $SLOT = "_" ] && {
-    SLOT=
-    isABDevice=false
-    SYSTEM=/system
-    SYSTEM2=/system
-    CACHELOC=/cache
-  } || {
-    isABDevice=true
-    SYSTEM=/system_root/system
-    SYSTEM2=/system
-    CACHELOC=/data/cache
-  }
+if [ -d /system_root ]; then
+  isABDevice=true
+  SYSTEM=/system_root/system
+  SYSTEM2=/system
+  CACHELOC=/data/cache
+else
+  isABDevice=false
+  SYSTEM=/system
+  SYSTEM2=/system
+  CACHELOC=/cache
 fi
+[ -z "$isABDevice" ] && { echo "Something went wrong!"; exit 1; }
 
 #=========================== Set Busybox up
 # Variables:
